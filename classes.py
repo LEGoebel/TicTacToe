d7c4caa36443cef404d37d10ad1bb50ab943da82
LEGoebel <l_goebel@outlook.de>
2024-08-15 23:15:20 +0200

    15.08.24 Initial commit - Tic Tac Toe implementiert (Zeitlimit war 1 Stunde, nur Dokumentation von Tkinter zum Lernen, wie man GUIs erstellt)

---
+++ classes.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToeGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Tic Tac Toe")
+
+        self.board = [" " for i in range(9)]
+        self.current_winner = None
+        self.current_player = "X"
+
+        self.buttons = []
+        for i in range(9):
+            button = tk.Button(self.root, text = " ", font=("normal", 40), width=5, height=2,
+                               command=lambda i = i: self.on_click(i))
+            button.grid(row = i // 3, column = i % 3)
+            self.buttons.append(button)
+
+    def on_click(self, index):
+            if self.board[index] == " " and not self.current_winner:
+                self.board[index] = self.current_player
+                self.buttons[index].config(text = self.current_player)
+
+            if self.check_winner(self.current_player):
+                self.current_winner = self.current_player
+                messagebox.showinfo("Tic Tac Toe", f"Player {self.current_player} wins!")
+                self.reset_board()
+            elif " " not in self.board:
+                messagebox.showinfo("Tic Tac Toe", "It's a tie!")
+                self.reset_board()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"
+
+    def check_winner(self, player):
+            win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8),  # Reihen
+                              (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Spalten
+                              (0, 4, 8), (2, 4, 6)]  # Diagonalen
+            for condition in win_conditions:
+                if self.board[condition[0]]==self.board[condition[1]]==self.board[condition[2]]==player:
+                    return True
+            return False
+        
+    def reset_board(self):
+            self.board = [" " for _ in range(9)]
+            for button in self.buttons:
+                button.config(text=" ")
+            self.current_winner = None
+            self.current_player = "X"
---
+++ main.py
@@ -0,0 +1,8 @@
+from classes import *
+
+def main():
+    root = tk.Tk()
+    game = TicTacToeGUI(root)
+    root.mainloop()
+
+main()
